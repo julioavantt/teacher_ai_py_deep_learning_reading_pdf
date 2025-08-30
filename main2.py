6f692a4b8646085f9b03263e8b08e3ca22ffdafd
unicorn <unicorn@flying.com>
2025-08-30 20:35:45 -0300

    mvp.1

--- main2.py
+++
@@ -1,3 +0,0 @@
-from data_loader import cargar_fragmentos
-from embeddings_generator2 import generar_embeddings
-print("pipi2")
--- test.py
+++
@@ -1,4 +0,0 @@
-from keras import models
-
-# Inspeccionar el contenido de tensorflow
-print(dir(models))
